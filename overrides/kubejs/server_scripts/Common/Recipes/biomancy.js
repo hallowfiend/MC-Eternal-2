9b27fe785d9d8ed03a02f096be728dd87689dfb3
adam <57104144+adam9899@users.noreply.github.com>
2026-05-10 23:13:45 +0100

    removed broken output

--- overrides/kubejs/server_scripts/Common/Recipes/biomancy.js
+++ overrides/kubejs/server_scripts/Common/Recipes/biomancy.js
@@ -1,12 +1,12 @@
 ServerEvents.recipes(event => {
   
-  event.recipes.biomancy.decomposing('ars_nouveau:source_gem', [OutputItem.of('biomancy:gem_fragments').withRolls(2, 4), Item.of('biomancy:exotic_dust').withRolls(1, 2)]).processingTime(20 * 15).nutrientsCost(3)
-  event.recipes.biomancy.decomposing('hexcasting:amethyst_dust', [OutputItem.of('biomancy:gem_fragments').withRolls(0, 1)]).processingTime(20 * 4).nutrientsCost(2)
+  event.recipes.biomancy.decomposing('ars_nouveau:source_gem', [OutputItem.of('biomancy:gem_fragments',4), Item.of('biomancy:exotic_dust').withRolls(1, 2)]).processingTime(20 * 15).nutrientsCost(3)
+  event.recipes.biomancy.decomposing('hexcasting:amethyst_dust', [OutputItem.of('biomancy:gem_fragments', 1)]).processingTime(20 * 4).nutrientsCost(2)
   event.recipes.biomancy.decomposing('hexcasting:charged_amethyst', [OutputItem.of('biomancy:gem_fragments', 6)]).processingTime(20 * 6).nutrientsCost(6)
-  event.recipes.biomancy.decomposing('irons_spellbooks:arcane_essence', [OutputItem.of('biomancy:exotic_dust').withRolls(0, 1)]).processingTime(20 * 2).nutrientsCost(2)
-  event.recipes.biomancy.decomposing('goety:ectoplasm', [OutputItem.of('biomancy:exotic_dust').withRolls(0, 3), Item.of('biomancy:bio_lumens').withRolls(1, 3)]).processingTime(20 * 8).nutrientsCost(8)
-  event.recipes.biomancy.decomposing('goety:spider_egg', [OutputItem.of('biomancy:bile').withRolls(1, 3), Item.of('biomancy:organic_matter').withRolls(0, 2), Item.of('biomancy:hormone_secretion', 1)]).processingTime(20 * 3).nutrientsCost(3)
-  event.recipes.biomancy.decomposing('goety:venomous_fang', [OutputItem.of('biomancy:toxin_gland').withRolls(0, 1), Item.of('biomancy:toxin_extract', 1), Item.of('biomancy:sharp_fang').withRolls(0, 1)]).processingTime(20 * 6).nutrientsCost(3)
+  event.recipes.biomancy.decomposing('irons_spellbooks:arcane_essence', [OutputItem.of('biomancy:exotic_dust', 1)]).processingTime(20 * 2).nutrientsCost(2)
+  event.recipes.biomancy.decomposing('goety:ectoplasm', [OutputItem.of('biomancy:exotic_dust', 3), Item.of('biomancy:bio_lumens', 3)]).processingTime(20 * 8).nutrientsCost(8)
+  event.recipes.biomancy.decomposing('goety:spider_egg', [OutputItem.of('biomancy:bile', 3), Item.of('biomancy:organic_matter').withRolls(0, 2), Item.of('biomancy:hormone_secretion', 1)]).processingTime(20 * 3).nutrientsCost(3)
+  event.recipes.biomancy.decomposing('goety:venomous_fang', [OutputItem.of('biomancy:toxin_gland', 1), Item.of('biomancy:toxin_extract', 1), Item.of('biomancy:sharp_fang', 1)]).processingTime(20 * 6).nutrientsCost(3)
   
 	event.recipes.biomancy.bio_forging([Item.of('biomancy:bone_fragments', 8), 'biomancy:mob_claw', 'ars_nouveau:earth_essence'], 'ars_nouveau:wilden_horn', 'biomancy:components').nutrientsCost(20)
   event.recipes.biomancy.bio_forging([Item.of('biomancy:bone_fragments', 8), 'biomancy:mob_fang', 'ars_nouveau:water_essence'], 'ars_nouveau:wilden_spike', 'biomancy:components').nutrientsCost(20)
