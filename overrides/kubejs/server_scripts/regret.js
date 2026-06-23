7132f4751fc2982e994f83b0497238b5fb5cc6de
adam <57104144+adam9899@users.noreply.github.com>
2026-06-23 17:47:39 +0100

    scroll of regret

--- overrides/kubejs/server_scripts/regret.js
+++ overrides/kubejs/server_scripts/regret.js
@@ -1,21 +1,29 @@
 ItemEvents.rightClicked('kubejs:regret_scroll', event => {
     const player = event.player
+    player.sendData('regret_scroll_activate', {
+        item: 'kubejs:regret_scroll'
+    })
 
     player.runCommandSilent('gamestage remove @s expert')
     player.runCommandSilent('gamestage add @s regret_scroll_active')
+
     player.tell(
-        Text.of('You have returned to Normal Mode, you may enter Expert Mode by visiting the Nether again whenever you feel prepared!')
+        Text.of(
+            'You have returned to Normal Mode, you may enter Expert Mode by visiting the Nether again whenever you feel prepared!'
+        )
             .gold()
             .italic(true)
     )
+
     event.item.count--
 })
 
 ServerEvents.tick(event => {
-if (event.server.tickCount % 60 !== 0) return 
+    if (event.server.tickCount % 60 !== 0) return
     event.server.players.forEach(player => {
         if (!player.stages.has('regret_scroll_active')) return
-     if (player.level.dimension == 'minecraft:the_nether') {
+
+        if (player.level.dimension == 'minecraft:the_nether') {
             player.runCommandSilent('gamestage add @s expert')
             player.runCommandSilent('gamestage remove @s regret_scroll_active')
             player.tell(
