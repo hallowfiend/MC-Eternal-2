791117a4e4809504e8e68a642900cb306c69b3dc
adam <57104144+adam9899@users.noreply.github.com>
2026-06-29 00:46:13 +0100

    Update heart.js

--- overrides/kubejs/startup_scripts/heart.js
+++ overrides/kubejs/startup_scripts/heart.js
@@ -1,21 +1,23 @@
+// lorkan heartd startup
 ItemEvents.modification(event => {
     event.modify('kubejs:lorkhan_heart', item => {
         item.attachCuriosCapability(
             CuriosJSCapabilityBuilder.create()
                 .curioTick((slotContext, stack) => {
-                    const player = slotContext.entity()
-                    if (player.level.isClientSide()) return
-                    const lowhp = player.health <= player.maxHealth / 2
-                    if (player.stages.has('heart_cooldown')) return
-                    if (!lowhp) return
-                    player.potionEffects.add('irons_spellbooks:heartstop', 400, 0, false, false)
-                    player.potionEffects.add('irons_spellbooks:hastened', 400, 3, false, false)
+        
+        const player = slotContext.entity()
+        if (player.level.isClientSide()) return
+        const heartstop = player.health <= player.maxHealth / 2
+        if (player.stages.has('curio_cooldown')) return
+        if (!heartstop) return
+        player.potionEffects.add('irons_spellbooks:heartstop', 400, 0, true, false)
+        player.potionEffects.add('irons_spellbooks:hastened', 400, 3, false, false)
 
-                    player.sendData('lorkhan_heart_activate', {})
-                    player.stages.add('heart_cooldown')
+        player.sendData('lorkhan_heart_activate', {})
+                    player.stages.add('curio_cooldown')
                     player.server.scheduleInTicks(12000, () => {
                         if (player) {
-                            player.stages.remove('heart_cooldown')
+                            player.stages.remove('curio_cooldown')
                         }
                     })
                 })
