8e7b1201e53d0f8cb23d6f6d3fa8b2f19cc77f5c
adam <57104144+adam9899@users.noreply.github.com>
2026-06-29 00:46:35 +0100

    Update heart.js

--- overrides/kubejs/startup_scripts/heart.js
+++ overrides/kubejs/startup_scripts/heart.js
@@ -10,7 +10,7 @@ ItemEvents.modification(event => {
         const heartstop = player.health <= player.maxHealth / 2
         if (player.stages.has('curio_cooldown')) return
         if (!heartstop) return
-        player.potionEffects.add('irons_spellbooks:heartstop', 400, 0, true, false)
+        player.potionEffects.add('irons_spellbooks:heartstop', 400, 0, false, false)
         player.potionEffects.add('irons_spellbooks:hastened', 400, 3, false, false)
 
         player.sendData('lorkhan_heart_activate', {})
